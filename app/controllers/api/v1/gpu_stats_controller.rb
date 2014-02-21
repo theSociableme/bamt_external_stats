d074efe6ee297e1dd557668d90389d3f11efa969
Mark Wagner <mwagner72@gmail.com>
2014-02-20 22:07:56 -0600

    Fixed stylesheets

--- app/controllers/api/v1/gpu_stats_controller.rb
+++ app/controllers/api/v1/gpu_stats_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::GpuStatsController < ApplicationController
+class Api::V1::GpuStatsController < Api::V1::BaseController
   inherit_resources
   skip_before_filter :verify_authenticity_token
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 MiningStats::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  ActiveAdmin.routes(self)
+
   namespace "api/v1", :as=>:api_v1 do
     resources :gpu_stats
   end
+
+  root :to => "gpu_stats#index"
 end
