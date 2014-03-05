7fc5850d4ed1df8ebf9abadfe3297317ed5d3e6f
Mark Wagner <mwagner72@gmail.com>
2014-03-04 20:45:33 -0600

    Updated graphs

--- app/controllers/gpu_stats_controller.rb
+++ app/controllers/gpu_stats_controller.rb
@@ -1,6 +1,6 @@
 class GpuStatsController < ApplicationController
   def index
-    @gpu_stats = GpuStat.where("created_at > ?", DateTime.now - 2.hours )
+    @gpu_stats = GpuStat.order('created_at DESC').limit(30)
     @gpu_stats_by_display = @gpu_stats.group_by(&:display)
   end
 end
