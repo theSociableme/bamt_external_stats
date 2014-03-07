78a0b27ce0badb6e1c3f99abc962c67b215d9382
Mark Wagner <mwagner72@gmail.com>
2014-03-07 07:47:13 -0600

    more graph points

--- app/controllers/gpu_stats_controller.rb
+++ app/controllers/gpu_stats_controller.rb
@@ -1,6 +1,6 @@
 class GpuStatsController < ApplicationController
   def index
-    @gpu_stats = GpuStat.last(60)
+    @gpu_stats = GpuStat.last(120)
     @gpu_stats_by_display = @gpu_stats.group_by(&:display)
   end
 end
